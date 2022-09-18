d18a3f5a7356954c1ae6fb01959f3ed4642b923c
Pedro Cosatto <you@example.com>
2022-09-18 14:59:21 -0300

    market data set added

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Stock Market
+#'
+#' @description The weekly rates of return for five stocks (p =5) listed on
+#'  the New York Stock Exchange are given.
+#'
+#' @format Numeric data frame with 103 rows and 5 columns.
+#'
+#' \describe{
+#'   \item{JPMorgan_Bank}{}
+#'   \item{Citibank_Bank}{}
+#'   \item{WellsFargo_Bank}{}
+#'   \item{RoyalDutchShell_Oil}{}
+#'   \item{ExxonMobil_Oil}{}
+#' }
+#'
+#' @details Each observation is calculated as the price
+#' variation over a week, relative to the price at the
+#' start of the week. 103 consecutive rows are given.
+#'
+#' @source \url{www.prenhall.com/statistics}
+"market"
